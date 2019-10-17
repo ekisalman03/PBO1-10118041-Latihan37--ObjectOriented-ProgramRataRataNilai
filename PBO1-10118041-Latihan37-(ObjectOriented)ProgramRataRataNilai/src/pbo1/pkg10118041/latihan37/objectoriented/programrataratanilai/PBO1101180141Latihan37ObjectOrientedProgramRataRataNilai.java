cc6bb0684b10a4b77c867cfb87db9093df25e92a
ekisalman03 <55838966+ekisalman03@users.noreply.github.com>
2019-10-17 22:42:26 +0700

    Add files via upload

---
+++ PBO1-10118041-Latihan37-(ObjectOriented)ProgramRataRataNilai/src/pbo1/pkg10118041/latihan37/objectoriented/programrataratanilai/PBO1101180141Latihan37ObjectOrientedProgramRataRataNilai.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118041.latihan37.objectoriented.programrataratanilai;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 
+ * NAMA     : Eki salman sya'bani
+ * KELAS    : IF-1
+ * NIM      : 10118041
+ * Deskripsi Program : Program ini untuk menampilkan tampilan program rata-rata 
+ *                     nilai.
+ */
+public class PBO1101180141Latihan37ObjectOrientedProgramRataRataNilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         Mahasiswa mah = new Mahasiswa();
+         Scanner scn = new Scanner(System.in);
+
+         int n;
+         System.out.print("Masukkan Banyaknya Mahasiswa : ");
+         n = scn.nextInt();
+
+         mah.HitungTotal(mah.nilaiMhs, n);
+         mah.HitungRataRataNilaiMhs(mah.totalNilaiMhs, n);
+
+        System.out.println("\nMaka, Rata-rata Nilainya adalah : "+mah.HitungRataRataNilaiMhs(mah.totalNilaiMhs,n));
+        System.out.println("Developed by : Tias Gumelar T");
+
+    }
+    
+}
---
+++ PBO1-10118041-Latihan37-(ObjectOriented)ProgramRataRataNilai/src/pbo1/pkg10118041/latihan37/objectoriented/programrataratanilai/PBO1101180141Latihan37ObjectOrientedProgramRataRataNilaiImpl.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118041.latihan37.objectoriented.programrataratanilai;
+
+
+public class PBO1101180141Latihan37ObjectOrientedProgramRataRataNilaiImpl extends PBO1101180141Latihan37ObjectOrientedProgramRataRataNilai {
+    
+}
